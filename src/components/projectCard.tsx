64d3c37dce090de84626a59efe41d4be4ce4ecad
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2025-04-14 14:56:06 +0530

    chore: data

--- src/components/projectCard.tsx
+++ src/components/projectCard.tsx
@@ -24,7 +24,7 @@ const ProjectCard: FC<ProjectCardProps> = ({ project }) => {
 				className="group relative -mx-3 w-full transform rounded-lg px-3 py-2 transition-all duration-300 ease-in-out md:hover:scale-105 md:hover:bg-orangeBg">
 				<div className="flex items-center justify-between text-sm font-medium tracking-tight text-title md:group-hover:text-primary">
 					<div className="flex items-center">
-						<h3>{project.title}</h3>
+						<h3 className="font-bold">{project.title}</h3>
 						{project.tag && (
 							<div className="ml-2 rounded-full border border-emerald-700 bg-emerald-100 px-1.5 py-0.5 text-xs font-medium text-emerald-700">
 								<span>{project.tag}</span>
